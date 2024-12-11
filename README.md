21f385259a162ea00a0d1a69b6fcfbcea4230a33
Prithak koirala <prithakkoirala44@gmail.com>
2024-12-11 18:15:14 +0545

    les see

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-safety
+# Loaders
+Three basic loader types.
+
+## More
+- Displays three types of usable loaders on button-click.
+- Includes spinner, bouncer and bar.
+- Looks asthetic AF.
+
+## Made using
+
+- **HTML**: Basic skeleton for adding the divisions for loaders.
+- **CSS**: To style i.e add colors, depth and animations.
+- **JavaScript**: To hide and unhide other loaders when ne is active. 
+
+## Usage
+
+1. Just normal cloning 
+2. Open the folder
+3. Checkout the loaders on loader.html
+
+## Screenshot
+
+![bounce preview](bouncer.png)
+![bar preview](bar.png)
+![spinner preview](spinner.png)
+
--- loader/loader.js
+++ loader/loader.js
@@ -1,4 +1,21 @@
 function ShowSpinner(){
     document.getElementById('spinner').style.visibility="visible";
-    document.getElementById('bouncer').style.visibility="invisible";
+    document.getElementById('bouncer').style.visibility="hidden";
+    document.getElementById('bar').style.visibility="hidden";
+}
+
+function ShowBouncer(){
+    document.getElementById('bouncer').style.visibility="visible";
+    document.getElementById('spinner').style.visibility="hidden";
+    document.getElementById('bar').style.visibility="hidden";
+}
+
+function ShowBar(){
+    document.getElementById('bouncer').style.visibility="hidden";
+    document.getElementById('spinner').style.visibility="hidden";
+    document.getElementById('bar').style.visibility="visible";
+}
+
+function loading(){
+    alert("It's loading bebs.");
 }
